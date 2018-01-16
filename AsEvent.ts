dea5d73f73a0bf5825dfe45f1c44c06e9fc0014d
pon87 <chitose87@gmail.com>
2018-01-16 17:39:46 +0900

    setting change

--- AsEvent.ts
+++ AsEvent.ts
@@ -1,14 +1,14 @@
-/**
- *
- */
-import {AsEventDispatcher} from "./AsEventDispatcher";
+namespace pon87.event {
+	/**
+	 * @version 0.0.1
+	 * @author chitose
+	 */
+	export class AsEvent {
+		type: string;
+		current: AsEventDispatcher;
 
-export class AsEvent {
-    type: string;
-    current: AsEventDispatcher;
-
-    constructor(type: string) {
-        this.type = type;
-    }
+		constructor(type: string) {
+			this.type = type;
+		}
+	}
 }
-
--- AsEventDispatcher.ts
+++ AsEventDispatcher.ts
@@ -1,51 +1,76 @@
-/**
- *
- */
-import {AsEvent} from "./AsEvent";
+namespace pon87.event {
+	/**
+	 * @version 0.0.1
+	 * @author chitose
+	 */
+	export class AsEventDispatcher {
 
-export class AsEventDispatcher {
-	hasListener: { [key: string]: any[] } = {};
+		hasListener: { [key: string]: any[] } = {};
 
-	listener(type: string, handler: (AsEvent: any) => void): AsEventDispatcher | any {
-		if (!this.hasListener[type]) this.hasListener[type] = [];
-		this.hasListener[type].push(handler);
-		return this;
-	}
-
-	onceListener(type: string, handler: (AsEvent: any) => void): AsEventDispatcher | any {
-		let func = (event: AsEvent) => {
-			this.removeListener(type, func);
-			handler(event);
-		};
-		return this.listener(type, func);
-	}
-
-	removeListener(type: string, handler?: (AsEvent: any) => void): AsEventDispatcher | any {
-		let handlers = this.hasListener[type];
-		if (handlers) {
-			for (let i = handlers.length - 1; i >= 0; i--) {
-				if (handlers[i] == handler) {
-					handlers.splice(i, 1);
-				}
-			}
-		} else {
-			handlers = [];
-		}
-		this.hasListener[type] = handlers;
-		return this;
-	}
-
-	dispatchEvent(event: AsEvent): AsEventDispatcher | any {
-		if (!this.hasListener[event.type]) {
+		/**
+		 *
+		 * @param {string} type
+		 * @param {(AsEvent: any) => void} handler
+		 * @returns {AsEventDispatcher | any}
+		 */
+		listener(type: string, handler: (AsEvent: any) => void): AsEventDispatcher | any {
+			if (!this.hasListener[type]) this.hasListener[type] = [];
+			this.hasListener[type].push(handler);
 			return this;
 		}
-		if (event.type in this.hasListener == false) return this;
-		let handlers = this.hasListener[event.type];
 
-		event.current = this;
-		for (let handler of handlers) {
-			handler(event);
+		/**
+		 *
+		 * @param {string} type
+		 * @param {(AsEvent: any) => void} handler
+		 * @returns {AsEventDispatcher | any}
+		 */
+		onceListener(type: string, handler: (AsEvent: any) => void): AsEventDispatcher | any {
+			let func = (event: AsEvent) => {
+				this.removeListener(type, func);
+				handler(event);
+			};
+			return this.listener(type, func);
 		}
-		return this;
+
+		/**
+		 *
+		 * @param {string} type
+		 * @param {(AsEvent: any) => void} handler
+		 * @returns {AsEventDispatcher | any}
+		 */
+		removeListener(type: string, handler?: (AsEvent: any) => void): AsEventDispatcher | any {
+			let handlers = this.hasListener[type];
+			if (handlers) {
+				for (let i = handlers.length - 1; i >= 0; i--) {
+					if (handlers[i] == handler) {
+						handlers.splice(i, 1);
+					}
+				}
+			} else {
+				handlers = [];
+			}
+			this.hasListener[type] = handlers;
+			return this;
+		}
+
+		/**
+		 *
+		 * @param {AsEvent} event
+		 * @returns {AsEventDispatcher | any}
+		 */
+		dispatchEvent(event: AsEvent): AsEventDispatcher | any {
+			if (!this.hasListener[event.type]) {
+				return this;
+			}
+			if (event.type in this.hasListener == false) return this;
+			let handlers = this.hasListener[event.type];
+
+			event.current = this;
+			for (let handler of handlers) {
+				handler(event);
+			}
+			return this;
+		}
 	}
 }
